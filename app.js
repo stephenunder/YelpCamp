c8aeaeee2809ba001bde43d631bf46153127a475
Stephen Underwood <41934385+stephenunder@users.noreply.github.com>
2019-05-11 11:27:53 -0500

    add moment to campgrounds view

--- app.js
+++ app.js
@@ -25,6 +25,8 @@ app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 app.use(methodOverride("_method"));
 app.use(flash());
+// require moment
+app.locals.moment = require("moment");
 // seedDB(); // seed the database
 
 // PASSPORT CONFIGURATION
